9dd19e6171af4499ba1b04baa07b884a5aee96ea
Eman Ramadan <eman01148597034@gmail.com>
2025-04-09 09:59:09 +0200

    Amal UnitofWork & Repos

---
+++ Sanayii/Repository/ArtisanRepository.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sanayii.Core.Entities;
+using Snai3y.Repository.Data;
+
+namespace Sanayii.Repository
+{
+    public class ArtisanRepository : GenericRepository<Artisan>
+    {
+        public ArtisanRepository(SanayiiContext db) : base(db) { }
+
+        public  List<Artisan> GetTopRatedArtisans()
+        {
+            return db.Artisans.OrderByDescending(a => a.Rating).ToList();
+        }
+    }
+}
