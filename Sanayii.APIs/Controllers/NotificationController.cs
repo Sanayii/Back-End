7abbf3c8ff4d01aa63d3d528aebb71eb76b44486
Eman Ramadan <eman01148597034@gmail.com>
2025-04-27 15:18:55 +0300

    Add NotificationController & Fun who BroadCasting

---
+++ Sanayii.APIs/Controllers/NotificationController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Sanayii.APIs.Controllers;
+using Sanayii.Core.Entities;
+using Sanayii.Service.Hubs;
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class NotificationController : ControllerBase
+{
+    private readonly IHubContext<NotificationHub> _hubContext;
+    private readonly ILogger<ChatController> _logger;
+
+    public NotificationController(IHubContext<NotificationHub> hubContext, ILogger<ChatController> _logger)
+    {
+        _hubContext = hubContext;
+        this._logger = _logger;
+    }
+
+    [HttpPost("send")]
+    public async Task<IActionResult> SendNotification([FromBody] Notification notification)
+    {
+        if (notification == null)
+        {
+            return BadRequest("Notification data is required.");
+        }
+
+        // Broadcast the notification to client
+        await _hubContext.Clients.All.SendAsync("ReceiveNotification", notification);
+
+        // Log the notification action
+        _logger.LogInformation($"Notification sent to CustomerId={notification.UserId}: Your service request status has been updated to: {notification.Content}");
+
+        return Ok();
+    }
+}
