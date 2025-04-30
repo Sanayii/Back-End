053df66ed1f4826ec126bcb61f12b979f1cf01fd
AyaMahmoud <mahmmoudaya5@gmail.com>
2025-04-30 21:20:53 +0300

    AddCustomerRequests-Endpoint

---
+++ Sanayii.Core/DTOs/CustomerDTOs/CustomerServiceRequestDto.cs
@@ -0,0 +1,23 @@
+﻿using Sanayii.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanayii.Core.DTOs.CustomerDTOs
+{
+    public class CustomerServiceRequestDto
+    {
+        public DateTime CreatedAt { get; set; }
+        public int ExecutionTime { get; set; }
+        public int Status { get; set; }
+        public int PaymentMethod { get; set; }      // NEW
+        public int PaymentAmount { get; set; }      // NEW
+        public string ServiceName { get; set; }
+        public string ArtisanName { get; set; }
+
+        
+
+    }
+}
