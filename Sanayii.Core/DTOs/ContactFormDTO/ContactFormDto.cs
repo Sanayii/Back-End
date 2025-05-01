47e022d19cd794e9ac93107d0933f9aa5229602f
rania504 <khalifarania6@gmail.com>
2025-05-02 02:19:50 +0300

    contact endpoint

---
+++ Sanayii.Core/DTOs/ContactFormDTO/ContactFormDto.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sanayii.Core.DTOs.ContactFormDTO
+{
+    public class ContactFormDto
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
